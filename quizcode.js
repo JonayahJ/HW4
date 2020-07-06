228c1ca6e8bda5108c00870ab46b0803e674db81
Jonayah <jonayah@thinkhalcyon.com>
2020-07-05 21:10:59 -0400

    pseudocodegit add .

---
+++ quizcode.js
@@ -0,0 +1,54 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+// predetermined data to include
+    // score counter starts at 0 (+1)
+    // timer (75 seconds)
+    // variables
+    // objects ---> question bank
+
+
+// click start button
+
+
+// timer starts
+
+
+// first question pops up
+
+
+// user input
+
+
+// if else statement
+    // if correct,
+    // then move on to next question
+    // else remove time and move on
+
+
+// when the timer reaches 0, then...
+    // timer is done
+    // all questions are asked
+
+
+// Game Over
+
+
+
+// Save your initials into the score card!
+
+
+
+// play again button
+

75a1102cb900d9a5c5c63ae6ec70914b06f39097
Jonayah <jonayah@thinkhalcyon.com>
2020-07-06 15:30:42 -0400

    added 10 horror questions, renamed js file, and started on scoring

---
+++ movietrivia.js
@@ -0,0 +1,69 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+// predetermined data to include
+    // score counter starts at 0 (+1)
+    // timer (75 seconds)
+    // variables
+    var score = 0
+
+    // objects ---> question bank
+
+    var movieTrivia = [
+        { q: "In \"Friday the 13th Part VI: Jason Lives\", how does Jason come back to life?", a: "Lightning" },
+        { q: "In \"The Shining\", what is the name of the hotel that Jack and family look after?", a: "The Overlook Hotel" },
+        { q: "In \"The Silence of the Lambs\", in which city is Dr. Lecter imprisoned?", a: "Baltimore" },
+        { q: "In \"A Nightmare on Elm Street\", which animal does Tina see in her dreams?", a: "Lamb" },
+        { q: "What is the name of the goat in \"The VVitch\"?", a: "Black Phillip" },
+        { q: "In \"Get Out\", what does Chris use to NOT get sent to the sunken place?", a: "cotton" },
+        { q: "What is the name of the demon in \"The Excorist\"?", a: "Pazuzu" },
+        { q: "What numbers keep appearing in \"Us\"?", a: "11:11" },
+        { q: "How many people died in \"The Babadook\"?", a: 0 },
+        { q: "Which movie has the tagline: \"We dare you to say his name 5 times\"?", a: "Candyman" },
+    ]
+
+
+// click start button
+
+
+// timer starts
+
+
+// first question pops up
+
+
+// user input
+
+
+// if else statement
+    // if correct,
+    // then move on to next question
+    // else remove time and move on
+
+
+// when the timer reaches 0, then...
+    // timer is done
+    // all questions are asked
+
+
+// Game Over
+
+
+
+// Save your initials into the score card!
+
+
+
+// play again button
+

15d40aa0c489eecd840131d6025b7cefe1c64b36
Jonayah <jonayah@thinkhalcyon.com>
2020-07-07 00:35:46 -0400

    console logging, extra questions, adding in some html

--- movietrivia.js
+++ movietrivia.js
@@ -1,15 +1,3 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-
 
 
 // predetermined data to include
@@ -21,24 +9,103 @@
     // objects ---> question bank
 
     var movieTrivia = [
-        { q: "In \"Friday the 13th Part VI: Jason Lives\", how does Jason come back to life?", a: "Lightning" },
-        { q: "In \"The Shining\", what is the name of the hotel that Jack and family look after?", a: "The Overlook Hotel" },
-        { q: "In \"The Silence of the Lambs\", in which city is Dr. Lecter imprisoned?", a: "Baltimore" },
-        { q: "In \"A Nightmare on Elm Street\", which animal does Tina see in her dreams?", a: "Lamb" },
-        { q: "What is the name of the goat in \"The VVitch\"?", a: "Black Phillip" },
-        { q: "In \"Get Out\", what does Chris use to NOT get sent to the sunken place?", a: "cotton" },
-        { q: "What is the name of the demon in \"The Excorist\"?", a: "Pazuzu" },
-        { q: "What numbers keep appearing in \"Us\"?", a: "11:11" },
-        { q: "How many people died in \"The Babadook\"?", a: 0 },
-        { q: "Which movie has the tagline: \"We dare you to say his name 5 times\"?", a: "Candyman" },
+        { 
+            question: "Who was the murderer in \"Friday the 13th\"?", 
+            choices: ["Pamela Vorhees", "Jason Vorhees", "Jeffrey Vorhees"],
+            correct: 0
+        },
+        
+        { 
+            question: "In \"The Shining\", what is the name of the hotel that Jack and family look after?", 
+            choices: ["The Redrum Inn", "The Overlook Hotel", "Hotel Artemis"],
+            correct: 1
+        },
+        
+        { 
+            question: "In \"The Silence of the Lambs\", in which city is Dr. Lecter imprisoned?", 
+            choices: ["Detroit", "Chicago", "Baltimore"],
+            correct: 2
+        },
+        
+        { 
+            question: "In \"A Nightmare on Elm Street\", which animal does Tina see in her dreams?", 
+            choices: ["calf", "doe", "Lamb"],
+            correct: 2 
+        },
+        
+        { 
+            question: "What is the name of the goat in \"The VVitch\"?", 
+            choices: ["Black Phillip", "Maurice", "Black Peter"], 
+            correct: 0
+        },
+        
+        { 
+            question: "In \"Get Out\", what does Chris use to NOT get sent to the sunken place?", 
+            choices: ["nylon", "cotton", "twill"],
+            correct: 1
+        },
+        
+        { 
+            question: "What is the name of the demon in \"The Excorist\"?", 
+            choices: ["Pazuzu", "Zazu", "Azazel"],
+            correct: 0
+        },
+        
+        { 
+            question: "What numbers keep appearing in \"Us\"?", 
+            choices: [42, "11:11", "22:22"], 
+            correct: 1
+        },
+        
+        { 
+            question: "How many people died in \"The Babadook\"?", 
+            choices: ["None", "a few", "a lot"],
+            correct: 0
+        },
+        
+        { 
+            question: "Which movie has the tagline: \"We dare you to say his name 5 times\"?", 
+            choices: ["The Muffin Man", "Freddy Kreuger", "Candyman"],
+            correct: 2
+        },
+
+        {
+            question: "Who was the first \"scream queen\" of a horror franchise?",
+            choices: ["Heather Langenkamp", "Jamie Lee Curtis", "Neve Campbell"],
+            correct: 1
+        },
+
+        {
+            question: "\"Hellraiser 3\" was filmed on which street in Greenboro, NC?",
+            choices: ["Elm Street", "Hell Drive", "Freddy Lane"],
+            correct: 0
+        },
+
+        {
+            question: "What was the name of the psychic in \"Poltergeist\" I and II?",
+            choices: ["Queen of Swords", "Miss Cleo", "Tangina"],
+            correct: 2
+        },
     ]
 
+    // console.log(movieTrivia)
 
 // click start button
-
+// addEventListener(onclick)
 
 // timer starts
-
+function setTime() { // has two conditions that need to be met (first condition is in seconds and the second condition is in milliseconds)
+    var timerInterval = setInterval(function() {
+      secondsLeft--; // must count down or it will continue forever
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval); // making the function stop
+        sendMessage();
+      }
+  
+    }, 1000); // equals 1 second because these are milliseconds
+  }
 
 // first question pops up
 

c26193dea269ee7dea4f37d23e3fca5eda221b9d
Jonayah <jonayah@thinkhalcyon.com>
2020-07-08 23:31:13 -0400

    added a README.md, edited it, added in more CSS, and fixing buttons.  CSS is still crap.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Horror Movie Trivia
+
+## To make this game, I did the following:
+
+* I started by creating all the files for the project: index.html, style,css, movietrivia.js, and README.md.
+
+* I pseudocoded what I needed to do to make this code work.
+
+* Then, I did some research and found some good trivia to add.
+
+*  I created the general layout of the html page and added the DOM elements to connect it to the JavaScript.
+
+* Next, I started to add the CSS (the background colors for the answer buttons refused to work like I wanted).
+
+* I then set global variables that I would need for the different aspects of the project: time, score, etc.
+
+* I then got started on the functions, which was the hardest part by far:
+
+    * Start button event
+    * Start game (with many if else statements to hide elements on the page)
+    * Setting and showing questions
+    * Getting the right answers and checking if they were correct
+    * Next button event
+    * Clearing old answers
+    * Countdown timer
+    * Scoreboard
+
+
--- movietrivia.js
+++ movietrivia.js
@@ -4,12 +4,15 @@ $(document).ready(function(){
 // predetermined data to include
     // DOM elements
     const startEl = document.getElementById("startBtn");
-    const nextButton = document.getElementById('nextBtn');
+    const introEl = document.getElementById("intro");
+    const nextEl = document.getElementById("nextBtn");
     const questionContainer = document.getElementById("questionContainer");
     // const triviaEl = document.getElementById("trivia");
 
     
     const questionEl = document.getElementById("question");
+    // const correctEl = document.getElementById("correct");
+    const answerButtons = document.getElementById("answerButtons");
     const choices0 = document.querySelector("#choices-0");
     const choices1 = document.querySelector("#choices-1");
     const choices2 = document.querySelector("#choices-2");
@@ -27,89 +30,132 @@ $(document).ready(function(){
     let questions = [
         {  //1
             question: "Who was the murderer in \"Friday the 13th\"?", 
-            choices: ["Pamela Vorhees", "Jason Vorhees", "Jeffrey Vorhees"],
-            correct: 0
+            answers: [
+                { text: "Pamela Vorhees", correct: true },
+                { text: "Jason Vorhees", correct: false },
+                { text: "Jeffrey Vorhees", correct: false },
+            ]
         },
         
         { //2
             question: "In \"The Shining\", what is the name of the hotel that Jack and family look after?", 
-            choices: ["The Redrum Inn", "The Overlook Hotel", "Hotel Artemis"],
-            correct: 1
+            answers: [
+                { text: "The Redrum Inn", correct: false },
+                { text: "The Overlook Hotel", correct: true },
+                { text: "Hotel Artemis", correct: false }
+            ]
         },
         
         { //3
             question: "In \"The Silence of the Lambs\", in which city is Dr. Lecter imprisoned?", 
-            choices: ["Detroit", "Chicago", "Baltimore"],
-            correct: 2
+            answers: [
+                { text: "Detroit", correct: false },
+                { text: "Chicago", correct: false },
+                { text: "Baltimore", correct: true },
+            ]
         },
         
         { //4
             question: "In \"A Nightmare on Elm Street\", which animal does Tina see in her dreams?", 
-            choices: ["Calf", "Doe", "Lamb"],
-            correct: 2 
+            answers: [
+                { text: "Calf", correct: false },
+                { text: "Doe", correct: false },
+                { text: "Lamb", correct: true },
+            ],
         },
         
         { //5
             question: "What is the name of the goat in \"The VVitch\"?", 
-            choices: ["Black Phillip", "Maurice", "Black Peter"], 
-            correct: 0
+            answers: [
+                { text: "Black Phillip", correct: true },
+                { text: "Maurice", correct: false },
+                { text: "Black Peter", correct: false },
+            ], 
         },
         
         { //6
             question: "In \"Get Out\", what does Chris use to NOT get sent to the sunken place?", 
-            choices: ["nylon", "cotton", "twill"],
-            correct: 1
+            answers: [
+                { text: "nylon", correct: false },
+                { text: "cotton", correct: true },
+                { text: "twill", correct: false },
+            ],
         },
         
         { //7
             question: "What is the name of the demon in \"The Excorist\"?", 
-            choices: ["Pazuzu", "Zazu", "Azazel"],
-            correct: 0
+            answers: [
+                { text: "Pazuzu", correct: true },
+                { text: "Zazu", correct: false },
+                { text: "Azazel", correct: false },
+            ],
         },
         
         { //8
             question: "What numbers keep appearing in \"Us\"?", 
-            choices: [42, "11:11", "22:22"], 
-            correct: 1
-        },
+            answers: [
+                { text: 42, correct: false },
+                { text: "11:11", correct: true },
+                { text: "22:22", correct: false },
+            ], 
+         },
         
         { //9
             question: "How many people died in \"The Babadook\"?", 
-            choices: ["None", "a few", "a lot"],
-            correct: 0
+            answers: [
+                { text: "none", correct: true },
+                { text: "a few", correct: false },
+                { text: "a lot", correct: false },
+            ],
         },
         
         { //10
             question: "Which movie has the tagline: \"We dare you to say his name 5 times\"?", 
-            choices: ["The Muffin Man", "Freddy Kreuger", "Candyman"],
-            correct: 2
+            answers: [
+                { text: "The Muffin Man", correct: false },
+                { text: "13 Ghosts", correct: false },
+                { text: "Candyman", correct: true },
+            ],
         },
 
         { //11
             question: "Who was the first \"scream queen\" of a horror franchise?",
-            choices: ["Heather Langenkamp", "Jamie Lee Curtis", "Neve Campbell"],
-            correct: 1
+            answers: [
+                { text: "Heather Langenkamp", correct: false },
+                { text: "Jamie Lee Curtis", correct: true },
+                { text: "Neve Campbell", correct: false },
+            ],
         },
 
         { //12
             question: "\"Hellraiser 3\" was filmed on which street in Greenboro, NC?",
-            choices: ["Elm Street", "Hell Drive", "Freddy Lane"],
-            correct: 0
+            answers: [
+                { text: "Elm Street", correct: true },
+                { text: "Hell Drive", correct: false },
+                { text: "Freddy Lane", correct: false },
+            ],
         },
 
         { //13
             question: "What was the name of the psychic in \"Poltergeist\" I and II?",
-            choices: ["Queen of Swords", "Miss Cleo", "Tangina"],
-            correct: 2
+            answers: [
+                { text: "Queen of Swords", correct: false },
+                { text: "Miss Cleo", correct: false },
+                { text: "Tangina", correct: true },
+            ],
         },
     ]
 
-    console.log(questions)
+    // console.log(questions)
 
 // FUNCTIONS =========================================================================
 
 // click start button to start game
 startEl.addEventListener("click", startGame)
+nextEl.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     // console.log("started");
@@ -121,13 +167,71 @@ function startGame() {
        startEl.style.display = "none"
    }
 
+   //hide intro text on press
+   if (introEl.style.display === "none") {
+       introEl.style.display = "block";
+  } else {
+       introEl.style.display = "none"
+  }
+
    //show questionContainer on press
    if (questionContainer.style.display === "block") {
     questionContainer.style.display = "none";
    } else {
     questionContainer.style.display = "block"
    }
-    
+   
+   // ask them questions
+   shuffledQuestions = questions.sort(() => Math.random() - .5);
+   currentQuestionIndex = 0
+
+   setNextQuestion ();
+};
+
+// shuffle questions
+function setNextQuestion (){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+};
+
+// show questions
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtons.appendChild(button)
+    });
+}
+
+// getting the right answers
+function selectAnswer(e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+};
+
+
+// check if it is correct?
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("incorrect")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("incorrect")
 }
 
 // countdown timer
@@ -143,60 +247,9 @@ function startGame() {
 // }
 
 // running questions
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
+let shuffledQuestions, currentQuestionIndex
 
-// function showQuestion() {
-//     console.log(questions[runningQuestion].question)
-    
-//     //empty out the question div and choices div
-    
-//     //1. create var
-//     var q = $("<p>");
-//     //<p></p>
-    
-//     //2. style it
-//     q.attr("id", "question");
-//      //<p id="question"></p>
-    
-//      q.text(questions[runningQuestion].question);
-//     //<p id="question">Question one Lives here</p>
-    
-//     //3. stick to page
-//     $("#trivia").append(q);
-    
-//     for(var i = 0;i < questions[runningQuestion].choices.length; i++){
-//         var btn = $("<button>");
-//         btn.attr("type","button");
-//         btn.attr("class","option btn btn-dark btn-lg btn-block")
-//         btn.attr("data-value", questions[runningQuestion].choices[i]);
-//         btn.text(questions[runningQuestion].choices[i]);
-//         $(".buttons").append(btn);
-//     }
-//     // onclick button
-//     $(".options").onclick= function(){
-//         //1.make sure btn triggers
-//         // alert("works");
-//         //2. grab the current value
-//         console.log($(this).text())
-//         //3. evaluate the currentvalue to the correct ans
-//     }
-// };
 
-// //startEl.style.display = "none";
-// showQuestion();
-// //triviaEl.style.display = "block"; // why block and not show?
-
-// $(".options").onclick= function(){
-   
-//     //1.make sure btn triggers
-//     // alert("works");
-//     //2. grab the current value
-//     console.log($(this).text())
-//     //3. evaluate the currentvalue to the correct ans
-   
-// }
-//console.log(showQuestion);
 
 // user input ===========================================================================
 

f0aa0c4999b58b424e99717b016edaab23435c50
Jonayah <jonayah@thinkhalcyon.com>
2020-07-10 16:38:09 -0400

    new timer

--- movietrivia.js
+++ movietrivia.js
@@ -12,7 +12,118 @@ $(document).ready(function(){
     const answerButtons = document.getElementById("answerButtons");
     
     const timerEl = document.getElementById("timer");
-    const scoreEl = document.getElementById("score");    
+    const scoreEl = document.getElementById("score");
+    
+    //timer
+
+
+// This code will run as soon as the page loads
+window.onload = function() 
+{
+  $("#loseTime").on("click", stopwatch.loseTime);
+  $("#stop").on("click", stopwatch.stop);
+  $("#reset").on("click", stopwatch.reset);
+  $("#start").on("click", stopwatch.start);
+};
+
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+//prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+// Our stopwatch object
+var stopwatch = 
+{
+
+  time: 180,
+
+
+  reset: function() 
+  {
+
+    stopwatch.time = 180;
+
+
+    // DONE: Change the "display" div to "00:00."
+    $("#timer").text("03:00");
+
+  },
+
+  start: function() 
+  {
+
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+    }
+  },
+  stop: function() 
+  {
+
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+  
+  count: function() 
+  {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    stopwatch.time--;
+    //console.log("time: "+stopwatch.time)
+
+    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+    //       and save the result in a variable.
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#timer").text(converted);
+  },
+  loseTime: function() 
+  {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    stopwatch.time-=10;
+    //console.log("time: "+stopwatch.time)
+
+    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+    //       and save the result in a variable.
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#timer").text(converted);
+  },
+  timeConverter: function(t) 
+  {
+    //t=1
+    //min0
+    //min=00
+    //sec1
+    //sec=01
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+};
+
+
+    //end of timer
 
 
     // variables
